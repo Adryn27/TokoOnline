cec5754c13519c121a626fb0a54fc2b494f0c79f
Your Name <you@example.com>
2024-12-14 16:54:51 +0700

    Pertemuan 12

--- resources/views/backend/v_layouts/app.blade.php
+++ resources/views/backend/v_layouts/app.blade.php
@@ -251,6 +251,29 @@
                   </li>
                 </ul>
               </li>
+              <li class="sidebar-item">
+                <a
+                  class="sidebar-link has-arrow waveseffect waves-dark"
+                  href="#"
+                  aria-expanded="false"
+                  ><i class="mdi mdi-shopping"></i
+                  ><span class="hide-menu">Laporan </span></a
+                >
+                <ul aria-expanded="false" class="collapse first-level">
+                  <li class="sidebar-item">
+                    <a href="{{ route('backend.laporan.formuser') }}" class="sidebar-link"
+                      ><i class="mdi mdi-chevron-right"></i
+                      ><span class="hide-menu"> User </span></a
+                    >
+                  </li>
+                  <li class="sidebar-item">
+                    <a href="{{ route('backend.laporan.formproduk') }}" class="sidebar-link"
+                      ><i class="mdi mdi-chevron-right"></i
+                      ><span class="hide-menu"> Produk </span></a
+                    >
+                  </li>
+                </ul>
+              </li>
             </ul>
           </nav>
           <!-- End Sidebar navigation -->
@@ -316,7 +339,7 @@
         <!-- footer -->
         <!-- ============================================================== -->
         <footer class="footer text-center">
-          Web Programming 2, Studi Kasus Toko Online
+          Copyright &copy; 2024 | Toko Online | by 
           <a href="https://www.instagram.com/adryn_w">Willy Andryan</a>.
         </footer>
         <!-- ============================================================== -->
